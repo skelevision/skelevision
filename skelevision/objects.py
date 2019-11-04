c132c6fd88661c7184a0afee6a76fb43557ba36e
Radu Coanda <andrei.coanda@gmail.com>
2019-11-04 16:28:53 +0100

    Moved equiv methods to TraceLog

--- skelevision/objects.py
+++ skelevision/objects.py
@@ -115,6 +115,7 @@ class TraceLog(MutableMapping):
             f.write(output)
         
         return True
+
     def never_together(self):
         """Returns a set of tuples, representing the pairs of the activities 
         which are never together in any of the traces.
@@ -140,6 +141,63 @@ class TraceLog(MutableMapping):
 
         return pairs
 
+    def equivalence(self):
+        R_eq_trace = dict()
+
+        for trace in self.__traces:
+            # Extract pairs in current trace
+            w = dict()
+            f2a = TraceLog.freq_2_activities(trace)
+            for s in f2a.values():
+                for pair in itertools.product(s, repeat=2):
+                    if pair[0] == pair[1]:
+                        continue
+                    if pair[0] not in w:
+                        w[pair[0]] = list()
+                    w[pair[0]].append(pair[1])
+
+            # Check if the relationships found till now still hold
+            for pair, value in R_eq_trace.items():
+                if value:
+                    if pair[0] not in w and pair[1] not in w:
+                        continue
+                    if pair[0] in w and pair[1] not in w[pair[0]]:
+                        R_eq_trace[pair] = False
+                        R_eq_trace[pair[::-1]] = False
+
+            # Check if there are new values to be added
+            for v0, values in w.items():
+                for v1 in values: 
+                    if (v0, v1) not in R_eq_trace:
+                        R_eq_trace[(v0, v1)] = True
+            
+        # Transform the dict to set
+        R_eq = set()
+        for pair, value in R_eq_trace.items():
+            if value:
+                R_eq.add(pair)
+
+        return R_eq
+
+    @staticmethod
+    def activity_2_freq(trace):
+        d = {}
+        for a in trace:
+            if a not in d:
+                d[a] = 0
+            d[a] += 1
+
+        return d
+
+    @staticmethod
+    def freq_2_activities(trace):
+        a2f = TraceLog.activity_2_freq(trace)
+        f2a = {}
+        for key, value in a2f.items():
+            if value not in f2a:
+                f2a[value] = set()
+            f2a[value].add(key)
+        return f2a
 
     @staticmethod
     def from_txt(filepath, delimiter=None, frequency_idx=0, first_activity_idx=2):
